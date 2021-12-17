8776f483e8ef594417e0ea132fd159496cb40567
unknown <altac-lancing@hotmail.com>
2021-12-18 01:55:05 +1000

    fuck you sexflixel

--- source/Paths.hx
+++ source/Paths.hx
@@ -1,10 +1,21 @@
 package;
 
+import sys.io.File;
 import flixel.FlxG;
 import flixel.graphics.frames.FlxAtlasFrames;
 import openfl.utils.AssetType;
 import openfl.utils.Assets as OpenFlAssets;
-
+import sys.FileSystem;
+#if haxe4
+import haxe.xml.Access;
+#else
+import haxe.xml.Fast as Access;
+#end
+import flash.geom.Rectangle;
+import flixel.graphics.FlxGraphic;
+import flixel.math.FlxRect;
+import flixel.graphics.frames.FlxFrame.FlxFrameAngle;
+import flixel.math.FlxPoint;
 class Paths
 {
 	inline public static var SOUND_EXT = #if web "mp3" #else "ogg" #end;
@@ -113,5 +124,62 @@ class Paths
 	inline static public function getPackerAtlas(key:String, ?library:String)
 	{
 		return FlxAtlasFrames.fromSpriteSheetPacker(image(key, library), file('images/$key.txt', library));
+	}
+
+	inline static public function getModSparrow(key:String){ //this is done like this cus fucking dumbass origonal thing used smth that unhardcoding cannot LOL
+		var Source:String = key;
+		var Description:String = key;
+		var graphic:FlxGraphic = FlxG.bitmap.add(Source);
+		if (graphic == null)
+			return null;
+
+		// No need to parse data again
+		var frames:FlxAtlasFrames = FlxAtlasFrames.findFrame(graphic);
+		if (frames != null)
+			return frames;
+
+		if (graphic == null || Description == null)
+			return null;
+
+		frames = new FlxAtlasFrames(graphic);
+
+		if (FileSystem.exists(Description))
+			Description = File.getContent(Description);
+
+		var data:Access = new Access(Xml.parse(Description).firstElement());
+
+		for (texture in data.nodes.SubTexture)
+		{
+			var name = texture.att.name;
+			var trimmed = texture.has.frameX;
+			var rotated = (texture.has.rotated && texture.att.rotated == "true");
+			var flipX = (texture.has.flipX && texture.att.flipX == "true");
+			var flipY = (texture.has.flipY && texture.att.flipY == "true");
+
+			var rect = FlxRect.get(Std.parseFloat(texture.att.x), Std.parseFloat(texture.att.y), Std.parseFloat(texture.att.width),
+				Std.parseFloat(texture.att.height));
+
+			var size = if (trimmed)
+			{
+				new Rectangle(Std.parseInt(texture.att.frameX), Std.parseInt(texture.att.frameY), Std.parseInt(texture.att.frameWidth),
+					Std.parseInt(texture.att.frameHeight));
+			}
+			else
+			{
+				new Rectangle(0, 0, rect.width, rect.height);
+			}
+
+			var angle = rotated ? FlxFrameAngle.ANGLE_NEG_90 : FlxFrameAngle.ANGLE_0;
+
+			var offset = FlxPoint.get(-size.left, -size.top);
+			var sourceSize = FlxPoint.get(size.width, size.height);
+
+			if (rotated && !trimmed)
+				sourceSize.set(size.height, size.width);
+
+			frames.addAtlasFrame(rect, sourceSize, offset, name, angle, flipX, flipY);
+		}
+
+		return frames;
 	}
 }
